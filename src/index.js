ae6de2dbce11e925617b13d596a341fcbb816886
Ricky Le <Ricky.Le88@gmail.com>
2020-11-16 21:04:07 -0500

    installed redux-persist to enable local storage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import reducer from './store/reducers';
@@ -9,10 +11,14 @@ import reducer from './store/reducers';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware()));
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,13 +1,23 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { calorieReducer } from './calorieReducer';
 import authReducer from './authReducer';
 import selectFoodReducer from './selectFoodReducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist:['calorieData', 'form', 'selectFood']
+}
+
+const rootReducer = combineReducers({
     calorieData: calorieReducer,
     form: formReducer,
     auth: authReducer,
     selectFood: selectFoodReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
